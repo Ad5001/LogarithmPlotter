400eda97335704281de6981186799fe237cba907
Ad5001 <mail@ad5001.eu>
2021-08-02 22:19:58 +0200

    It seems PySide2 wasn't detected, trying to see two potential sources.

--- build-windows.bat
+++ build-windows.bat
@@ -1,2 +1,2 @@
 python -m pip install -U pyinstaller
-pyinstaller --add-data "logplotter.svg;." --add-data "LogarithmPlotter/qml;qml" --noconsole LogarithmPlotter/__init__.py --icon=win/logarithmplotter.ico -n logarithmplotter
+pyinstaller --add-data "logplotter.svg;." --add-data "LogarithmPlotter/qml;qml" --noconsole LogarithmPlotter/logarithmplotter.py --icon=win/logarithmplotter.ico -n logarithmplotter
--- build-wine.sh
+++ build-wine.sh
@@ -3,4 +3,4 @@
 rm $(find . -name "*.qmlc")
 rm $(find . -name "*.pyc")
 wine python -m pip install -U pyinstaller
-wine pyinstaller --add-data "logplotter.svg;." --add-data "LogarithmPlotter/qml;qml" --noconsole LogarithmPlotter/__init__.py --icon=win/logarithmplotter.ico -n logarithmplotter
+wine pyinstaller --add-data "logplotter.svg;." --add-data "LogarithmPlotter/qml;qml" --noconsole LogarithmPlotter/logarithmplotter.py --icon=win/logarithmplotter.ico -n logarithmplotter
