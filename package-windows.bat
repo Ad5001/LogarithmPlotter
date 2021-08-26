21dc8a8b87c30c3ca7602c32ca71ff5ae6f5a8a3
Ad5001 <mail@ad5001.eu>
2021-08-26 18:29:23 +0200

    Imagine being so used to linux that you forget how backwards windows is.

--- package-windows.bat
+++ package-windows.bat
@@ -1,6 +1,6 @@
-XCOPY win/*.* dist/logarithmplotter/ /C /S /D /Y /I
-XCOPY README.md dist/logarithmplotter/ /C /D /Y
-XCOPY LICENSE.md dist/logarithmplotter/ /C /D /Y
+XCOPY win\*.* dist\logarithmplotter /C /S /D /Y /I
+XCOPY README.md dist\logarithmplotter /C /D /Y
+XCOPY LICENSE.md dist\logarithmplotter /C /D /Y
 rem Creating installer
-cd dist/logarithmplotter/
+cd dist\logarithmplotter
 "C:\Program Files (x86)\NSIS\makensis" installer.nsi
