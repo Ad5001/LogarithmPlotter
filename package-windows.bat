a00405d35c7c9be93873ba34a9b69f74ee520f05
Ad5001 <mail@ad5001.eu>
2021-08-26 18:26:36 +0200

    Windows doesn't have a cp command.

--- package-windows.bat
+++ package-windows.bat
@@ -1,4 +1,6 @@
-cp win/* README.md LICENSE.md dist/logarithmplotter/
+XCOPY win/*.* dist/logarithmplotter/ /C /S /D /Y /I
+XCOPY README.md dist/logarithmplotter/ /C /D /Y
+XCOPY LICENSE.md dist/logarithmplotter/ /C /D /Y
 rem Creating installer
 cd dist/logarithmplotter/
 "C:\Program Files (x86)\NSIS\makensis" installer.nsi
