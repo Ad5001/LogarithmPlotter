20f8d68f13f5defa5db4eba5c31438ac45bd949b
Ad5001 <mail@ad5001.eu>
2022-04-23 02:17:34 +0200

    Starting v0.2.1

--- scripts/package-macosx.sh
+++ scripts/package-macosx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname "$(readlink -f "$0" || realpath "$0")")/.."
 
-VERSION=0.2.0
+VERSION=0.2.1
 title="LogarithmPlotter v${VERSION} Setup"
 finalDMGName="LogarithmPlotter-v${VERSION}-setup.dmg"
 applicationName=LogarithmPlotter
